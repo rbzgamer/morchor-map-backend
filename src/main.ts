f28133eeef0404a4101c6a090ee4862b590b44e6
Autoz <76035467+rbzgamer@users.noreply.github.com>
2024-02-29 11:46:00 +0700

    fix port run to listen to deploy port

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    // Enable CORS with specific options
-    app.enableCors({
-      origin: 'http://localhost:3000', // Replace with the actual origin of your client application
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
+  // Enable CORS with specific options
+  app.enableCors({
+    origin: 'http://localhost:3000' || 'https://morchormap.netlify.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(5000);
+  // Use `PORT` provided in environment or default to 3000
+  const port = process.env.PORT || 3000;
+
+  // Listen on `port` and 0.0.0.0
+  async function bootstrap() {
+    // ...
+    (await app.listen(port, '0.0.0.0')) || (await app.listen(5000));
+  }
 }
 bootstrap();
